a9b4e9def1e4d8c8433ce0ad2e2ef9aa77cb3572
Alexander Gedz <gedz.buh@yandex.ru>
2024-08-08 14:07:56 +0300

    Для курса добавил сериализатор для вывода количества уроков

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,15 +1,26 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from materials.models import Course, Lesson
-
-
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
+        fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    lesson_count = SerializerMethodField()
+
+    def get_lesson_count(self, obj):
+        return obj.lesson_set.count()
+
+    class Meta:
+        model = Course
+        fields = ("name", "description", "lesson_count")
+
+
+class CourseSerializer(ModelSerializer):
+    class Meta:
+        model = Course
         fields = "__all__"
